8ab4007513562c599588b5f707afc2b2953ffe88
garzo94 <alexgarzo25@gmail.com>
2025-05-10 17:07:15 -0600

    data ingestion/transformation

--- src/exception.py
+++ src/exception.py
@@ -1,8 +1,8 @@
 import sys
 from src.logger import logging
-
+# CADA VEZ HAYA UN ERROR SE LLAMA A ESTA FUNCION
 def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
+    _,_,exc_tb=error_detail.exc_info() # Me da toda la info del error
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
      file_name,exc_tb.tb_lineno,str(error))
@@ -15,7 +15,7 @@ class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
-    
+    # when execute print this return
     def __str__(self):
         return self.error_message
     
